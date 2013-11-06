875798d25dd34d5032023111c0823c4cd8377ae2
Sergey Kashin <sergey.don.kashin@gmail.com>
2013-11-06 22:01:01 +0400

    AudioSource object is created to make it possible to customize in game sounds like Background Music.

---
+++ Assets/Scripts/Audio/GameMusic.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+public var mcLooped: boolean = false;
+public var mcAudioSource: AudioSource;
+public var mcAudioClip: AudioClip;
+
+
+function Start()
+{
+  if (mcAudioSource == null)
+  {
+    mcAudioSource = audio;
+  }
+  if (mcAudioSource)
+  {
+    mcAudioSource.clip = mcAudioClip;
+    mcAudioSource.loop = mcLooped;
+    mcAudioSource.Play();
+  }
+  else
+  {
+    Debug.LogError("audio source is not attached to this object!");
+  }
+}
+
+@script RequireComponent (AudioSource)
+@script AddComponentMenu ("GameSounds/In Game Music")
