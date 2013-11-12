14b4b36c582317bf074ad1bdf9c14257bb57d964
Sergey Kashin <sergey.don.kashin@gmail.com>
2013-11-13 00:03:53 +0400

    Bestiary Level implementation is started.

---
+++ Assets/Scripts/Bestiary/BestiaryControls.js
@@ -0,0 +1,76 @@
+﻿#pragma strict
+
+/*------------------------------------------ PUBLIC MEMBERS ------------------------------------------*/
+public var mcModelsTranforms: Transform[];
+
+// Since all our models have different sizes it is better to have a different
+public var mcCamerasDistancesToModels: float[];
+
+public var mcMoveTime: float = 1.0f;
+
+/*------------------------------------------ STYLES ------------------------------------------*/
+public var mcLeftArrowStyle: GUIStyle;
+public var mcRightArrowStyle: GUIStyle;
+
+/*------------------------------------------ SIZES ------------------------------------------*/
+public var mcArrowsSize: Vector2 = Vector2(50, 50);
+
+/*------------------------------------------ PRIVATE MEMBERS ------------------------------------------*/
+private var mcScreenWidth: float = 0.0f;
+private var mcScreenHeight: float = 0.0f;
+private var mcSpaceToButton: float = 30.0f;
+private var mcArrowsPos: Vector2 = Vector2(0, 0);
+
+private var mcModelIndex: int = 0;
+private var mcMoveToPosition: Vector3 = Vector3(0, 0, 0);
+
+/*------------------------------------------ MONOBEHAVIOR ------------------------------------------*/
+function Start()
+{
+  mcScreenWidth = Screen.width;
+  mcScreenHeight = Screen.height;
+  mcArrowsPos.x = mcScreenWidth - mcSpaceToButton - mcArrowsSize.x;
+  mcArrowsPos.y = mcScreenHeight - mcSpaceToButton - mcArrowsSize.y;
+  setModelIndex(0);
+  mcMoveToPosition.y = transform.position.y;
+}
+
+function Update()
+{
+  var currentPosition = transform.position;
+  transform.position = Vector3.Lerp(currentPosition, mcMoveToPosition, mcMoveTime);
+  mcMoveTime *= 0.1 * Time.deltaTime;
+}
+
+function OnGUI()
+{
+  GUI.BeginGroup(Rect(0, 0, mcScreenWidth, mcScreenHeight));
+    // Left Arrow Button
+    if (mcModelIndex > 0 && GUI.Button(Rect(mcSpaceToButton, mcArrowsPos.y, mcArrowsSize.x, mcArrowsSize.y), "", mcLeftArrowStyle))
+    {
+      setModelIndex(mcModelIndex - 1);
+    }
+    // Right Arrow Button
+    if (mcModelIndex < mcModelsTranforms.Length - 1 && GUI.Button(Rect(mcArrowsPos.x, mcArrowsPos.y, mcArrowsSize.x, mcArrowsSize.y), "", mcRightArrowStyle))
+    {
+      setModelIndex(mcModelIndex + 1);
+    }
+  GUI.EndGroup();
+}
+
+private function setModelIndex(index: int)
+{
+  if ( index < mcModelsTranforms.Length  && index >= 0)
+  {
+    var modelPos = mcModelsTranforms[index].position;
+    mcMoveToPosition.x = modelPos.x;
+    if (index < mcCamerasDistancesToModels.Length)
+    {
+      mcMoveToPosition.z = modelPos.z - mcCamerasDistancesToModels[index];
+    }
+    mcMoveTime = 1.0f;
+    mcModelIndex = index;
+  }
+}
+
+@script AddComponentMenu ("UI/Bestiary Control")
---
+++ Assets/Scripts/Bestiary/ModelBehavior.js
@@ -0,0 +1,25 @@
+﻿
+public var mcAnimationsNames: String[] = ["Attack", "Walk", "Dance"];
+
+function Start()
+{
+  animation.wrapMode = WrapMode.Loop;
+  animation.Play();
+}
+
+function Update()
+{
+  if (Input.GetButtonDown("Bestiary Action"))
+  {
+    var index = Random.Range(0, mcAnimationsNames.Length);
+    while (animation.IsPlaying(mcAnimationsNames[index]))
+    {
+      index = Random.Range(0, mcAnimationsNames.Length);
+    }
+    animation.CrossFade(mcAnimationsNames[index]);
+  }
+}
+
+@script RequireComponent (Animation)
+@script AddComponentMenu ("Mobs/Model Behavior")
+
